c3972a2e74aa663db6b00b58b2af8ff5ffd99ae9
Andrius <andriusjavait@gmail.com>
2017-01-16 18:56:14 +0000

    added MainServlet template code added MyServletRequestListener template code, updated DD servlet,mapping,listener

---
+++ ServletRequestL/src/com/example/listener/MyServletRequestListener.java
@@ -0,0 +1,18 @@
+package com.example.listener;
+import javax.servlet.*;
+
+public class MyServletRequestListener implements ServletRequestListener {
+
+	@Override
+	public void requestDestroyed(ServletRequestEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void requestInitialized(ServletRequestEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ ServletRequestL/src/com/example/servlet/MainServlet.java
@@ -0,0 +1,12 @@
+package com.example.servlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class MainServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request,
+			HttpServletResponse response)throws ServletException,IOException{
+		
+	}
+	
+}
