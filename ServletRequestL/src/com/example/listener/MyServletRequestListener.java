1a48b7cb851d1ab5c479557de4ef1bb42ac99320
Andrius <andriusjavait@gmail.com>
2017-01-16 19:35:52 +0000

    update MainServlet to get total request from listener, listner class update to make it work

--- ServletRequestL/src/com/example/listener/MyServletRequestListener.java
+++ ServletRequestL/src/com/example/listener/MyServletRequestListener.java
@@ -2,16 +2,31 @@ package com.example.listener;
 import javax.servlet.*;
 
 public class MyServletRequestListener implements ServletRequestListener {
-
+	// counter variable
+	public static int totalRequest = 0;
 	@Override
 	public void requestDestroyed(ServletRequestEvent event) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("\n*************************");
+        System.out.println("********** Request was done  ***************");
+        System.out.println("********** requestDestroyed() method run ***************");
+        System.out.println("********** End of method  ***************");
+        System.out.println("\n*************************");
 	}
 
 	@Override
 	public void requestInitialized(ServletRequestEvent event) {
-		// TODO Auto-generated method stub
+		System.out.println("\n*************************");
+        System.out.println("********** requestInitialized() method run ***************");
+        ServletContext sc = event.getServletContext();
+        // You can easly get request object from ServletRequest event:
+        ServletRequest request = event.getServletRequest();
+        System.out.println("We get ServletContext = " + sc + " object ");
+        System.out.println("We get ServletRequest = " + request + " object ");
+        System.out.println("Both from ServletRequestEvent!");
+        System.out.println("********** We got request ***************");
+        // we pre increament and show result:
+        System.out.println("********** Total request " + ++totalRequest + " ***************");
+        System.out.println("\n*************************");
 		
 	}
 
--- ServletRequestL/src/com/example/servlet/MainServlet.java
+++ ServletRequestL/src/com/example/servlet/MainServlet.java
@@ -1,12 +1,18 @@
 package com.example.servlet;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import com.example.listener.MyServletRequestListener;
+
 import java.io.*;
 
 public class MainServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request,
 			HttpServletResponse response)throws ServletException,IOException{
-		
+		 response.setContentType("text/html");
+	     PrintWriter out = response.getWriter();
+	     out.println("<html><title>ServletResponse</title>");
+	     out.println("<body><h3>Total request was made: " + MyServletRequestListener.totalRequest + "</h3></body>");
+	     out.println("</html>");
 	}
 	
 }
